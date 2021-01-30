45c98b3abdc9361bc432f3f6e17fe1802e44bf09
sanjar1311 <sanjarabduxamidov1311@gmail.com>
2021-01-30 13:44:51 +0500

    add cakes arrays

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,78 @@ var createElement = function (tagName, className, text) {
   return element;
 };
 
+
+let all = [];
+let cakes = [
+  {
+    title: `Cake Items`,
+    img: ``,
+    cost: `$5`
+  },
+  {
+    title: `Cake Items`,
+    img: ``,
+    cost: `$10`
+  },
+  {
+    title: `Cake Items`,
+    img: ``,
+    cost: `$15`
+  }
+];
+let cupcakes = [
+  {
+    title: `Cupcake Item`,
+    img: ``,
+    cost: `$5`
+  },
+  {
+    title: `Cupcake Item`,
+    img: ``,
+    cost: `$10`
+  },
+  {
+    title: `Cupcake Item`,
+    img: ``,
+    cost: `$10`
+  },
+];
+let sweets = [
+  {
+    title: `Sweets Item`,
+    img: ``,
+    cost: `$5`
+  },
+    {
+    title: `Sweets Item`,
+    img: ``,
+    cost: `$10`
+  },
+    {
+    title: `Sweets Item`,
+    img: ``,
+    cost: `$15`
+  },
+];
+let doughnuts = [
+  {
+    title: `Dougnut Item`,
+    img: ``,
+    cost: `$5`
+  },
+    {
+    title: `Dougnut Item`,
+    img: ``,
+    cost: `$10`
+  },
+    {
+    title: `Dougnut Item`,
+    img: ``,
+    cost: `$15`
+  },
+];
+
+
 let elNavOpenBtn = $_('.nav-open-btn');
 let elNav = $_('.nav');
 
